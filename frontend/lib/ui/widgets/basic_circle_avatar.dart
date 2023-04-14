e50ccfc2109ff35bec560e58df77d76ee5a2796c
ChrstnC <chrstncrrnd@gmail.com>
2023-04-14 22:39:04 +0200

    updated border on avatar

--- frontend/lib/ui/widgets/basic_circle_avatar.dart
+++ frontend/lib/ui/widgets/basic_circle_avatar.dart
@@ -12,7 +12,7 @@ class BasicCircleAvatar extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(radius + 100),
-          border: Border.all(color: Colors.white, width: 3)),
+          border: Border.all(color: Colors.white.withOpacity(0.2), width: 1)),
       child: CircleAvatar(
         backgroundColor: Colors.black,
         radius: radius,
